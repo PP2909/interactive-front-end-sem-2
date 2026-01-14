fe6f4a0cdd156ec067397f61e02bd065a51b2b67
PP2909 <pragyasinha561@gmail.com>
2026-01-14 10:54:20 +0530

    Add files via upload

---
+++ js2/Js Day 2/hi.py
@@ -0,0 +1,6 @@
+l=5
+for i in range(1,5):
+    i+=1
+    print(i)
+
+print(sum(i))
---
+++ js2/Js Day 2/script.js
@@ -0,0 +1,24 @@
+//map method
+
+let arr=[1,2,3,4,5]
+let resuklt=arr.map((x)=>x+2)
+console.log(resuklt)
+
+//filter method
+let scores=[12,7,22,15,32,6,4,27,11,16];
+let highScores=
+scores.filter((score)=>{
+    if(score>=15){
+        console.log(score,"pass")
+    }
+else{
+    console.log(score,"fail")
+}});
+
+//reduce method
+let numbers=[1,2,3,4,5];
+for (let i=0;i<numbers.length;i++){
+    numbers=numbers[i]+
+}
+
+//let sum = numbers.reduce((acc,current)+> console.log(current));
